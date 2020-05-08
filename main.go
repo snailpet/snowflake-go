a60561f7ffd8c6a64f7a2c6e707ea005500fc0c4
git <401061776@qq.com>
2020-05-08 18:26:07 +0800

    1

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 
 func main() {
 	totalNum := 1000000
-	currWoker := &IdWorker{}
-	err := currWoker.InitIdWorker(1023, 1023)
+	currWoker := &SnowFlake{}
+	err := currWoker.InitSnowFlake(1023, 1023)
 	if err != nil {
 		log.Fatalln(err)
 		return
@@ -44,7 +44,7 @@ func main() {
 	fmt.Println("重复数",repeatCount,len(dMap))
 }
 
-type IdWorker struct {
+type SnowFlake struct {
 	startTime             int64//开始时间的偏移量。
 	workerIdBits          uint//worker_id的长度
 	dataCenterIdBits      uint
@@ -62,65 +62,65 @@ type IdWorker struct {
 	idLock                *sync.Mutex
 }
 
-func (this *IdWorker) InitIdWorker(workerId, dataCenterId int64) error {
+func (sf *SnowFlake) InitSnowFlake(workerId, dataCenterId int64) error {
 	if workerId >=1024||dataCenterId>=1024  {
 		return errors.New("workerId or dataCenterId over max")
 	}
 	var baseValue int64 = -1
-	this.startTime = 1688927006000
-	this.workerIdBits = 5
-	this.dataCenterIdBits = 5
+	sf.startTime = 1688927006000
+	sf.workerIdBits = 5
+	sf.dataCenterIdBits = 5
 	// 这个是二进制运算，就是5 bit最多只能有31个数字，也就是说机器id最多只能是32以内
-	this.maxWorkerId = baseValue ^ (baseValue << this.workerIdBits)
+	sf.maxWorkerId = baseValue ^ (baseValue << sf.workerIdBits)
 	// 这个是一个意思，就是5 bit最多只能有31个数字，机房id最多只能是32以内
-	this.maxDataCenterId = baseValue ^ (baseValue << this.dataCenterIdBits)
-	this.sequenceBits = 12
-	this.workerIdLeftShift = this.sequenceBits //12
-	this.dataCenterIdLeftShift = this.workerIdBits + this.workerIdLeftShift//17
-	this.timestampLeftShift = this.dataCenterIdBits + this.dataCenterIdLeftShift//22
-	this.sequenceMask = baseValue ^ (baseValue << this.sequenceBits)
-	this.sequence = 0
-	this.lastTimestamp = -1
-	this.idLock = &sync.Mutex{}
+	sf.maxDataCenterId = baseValue ^ (baseValue << sf.dataCenterIdBits)
+	sf.sequenceBits = 12
+	sf.workerIdLeftShift = sf.sequenceBits //12
+	sf.dataCenterIdLeftShift = sf.workerIdBits + sf.workerIdLeftShift//17
+	sf.timestampLeftShift = sf.dataCenterIdBits + sf.dataCenterIdLeftShift//22
+	sf.sequenceMask = baseValue ^ (baseValue << sf.sequenceBits)
+	sf.sequence = 0
+	sf.lastTimestamp = -1
+	sf.idLock = &sync.Mutex{}
 
-	if this.workerId < 0 || this.workerId > this.maxWorkerId {
+	if sf.workerId < 0 || sf.workerId > sf.maxWorkerId {
 		return errors.New(fmt.Sprintf("workerId[%v] is less than 0 or greater than maxWorkerId[%v].", workerId, dataCenterId))
 	}
-	if this.dataCenterId < 0 || this.dataCenterId > this.maxDataCenterId {
+	if sf.dataCenterId < 0 || sf.dataCenterId > sf.maxDataCenterId {
 		return errors.New(fmt.Sprintf("dataCenterId[%d] is less than 0 or greater than maxDataCenterId[%d].", workerId, dataCenterId))
 	}
-	this.workerId = workerId
-	this.dataCenterId = dataCenterId
+	sf.workerId = workerId
+	sf.dataCenterId = dataCenterId
 	return nil
 }
 
-func (this *IdWorker) NextId() (int64, error) {
-	this.idLock.Lock()
+func (sf *SnowFlake) NextId() (int64, error) {
+	sf.idLock.Lock()
 	timestamp := time.Now().UnixNano()
 	//时间回拨直接return
-	if timestamp < this.lastTimestamp {
-		return -1, errors.New(fmt.Sprintf("Clock moved backwards.  Refusing to generate id for %d milliseconds", this.lastTimestamp-timestamp))
+	if timestamp < sf.lastTimestamp {
+		return -1, errors.New(fmt.Sprintf("Clock moved backwards.  Refusing to generate id for %d milliseconds", sf.lastTimestamp-timestamp))
 	}
 	//同一毫秒，生成对应的sn
-	if timestamp == this.lastTimestamp {
-		this.sequence +=1
-		if this.sequence > 4096 {
-			//如果是0，代表超出了
-			timestamp = this.tilNextMillis()
-			this.sequence = 0
+	if timestamp == sf.lastTimestamp {
+		sf.sequence +=1
+		if sf.sequence > 4096 {
+			//超出单毫秒限制数量，等待下一毫秒
+			timestamp = sf.waitNextMillis()
+			sf.sequence = 0
 		}
 	} else {
-		this.sequence = 0
+		sf.sequence = 0
 	}
 	//更新最后时间
-	this.lastTimestamp = timestamp
+	sf.lastTimestamp = timestamp
 
-	this.idLock.Unlock()
+	sf.idLock.Unlock()
 
-	id := ((timestamp - this.startTime) << this.timestampLeftShift) |
-		(this.dataCenterId << this.dataCenterIdLeftShift) |
-		(this.workerId << this.workerIdLeftShift) |
-		this.sequence
+	id := ((timestamp - sf.startTime) << sf.timestampLeftShift) |
+		(sf.dataCenterId << sf.dataCenterIdLeftShift) |
+		(sf.workerId << sf.workerIdLeftShift) |
+		sf.sequence
 
 	if id < 0 {
 		id = -id
@@ -128,10 +128,10 @@ func (this *IdWorker) NextId() (int64, error) {
 	return id, nil
 }
 
-func (this *IdWorker) tilNextMillis() int64 {
+func (sf *SnowFlake) waitNextMillis() int64 {
 	time.Sleep(time.Millisecond)//等待下一秒刷新
 	timestamp := time.Now().UnixNano()//当前时间
-	if timestamp <= this.lastTimestamp {//如果内存时间大于当前时间，将当前时间刷新为正确时间
+	if timestamp <= sf.lastTimestamp {//如果内存时间大于当前时间，将当前时间刷新为正确时间
 		timestamp = time.Now().UnixNano() / int64(time.Millisecond)
 	}
 	return timestamp
